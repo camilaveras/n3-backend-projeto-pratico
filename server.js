e2295f17e122f558b7035e730bef448890c0d0ac
Camila Veras Rocha <31225917+camilaveras@users.noreply.github.com>
2019-10-29 12:04:00 -0300

    Alterações
    Alterações

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const server = express();
 
 
 var arquivo = require('./lista.json'); //Desde que arquivo.js esteja na mesma pasta
-server.get('/', function (req, res) {
-    res.send(arquivo);
+    server.get('/', function (req, res) {
+        
+    
+        let concluidas = arquivo.filter((tarefa) => {
+            console.log(tarefa.id);           //aqui dentro tarefa existe
+            return tarefa.concluido === true;
+         })
+     
+         console.log(concluidas);
+         res.send(arquivo);
 
 })
 
-/*server.get('/', (req, res, next) => {
-res.json();
 
 
-});
-*/
+
 
 server.listen(3000);
-
