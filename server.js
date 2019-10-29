fa99aaeb6742a6a237e3426f54a8a0fd797e6c17
Camila Veras Rocha <31225917+camilaveras@users.noreply.github.com>
2019-10-29 15:39:41 -0300

    changes of functions and filters

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require ('express');
 const server = express();
 
+var arquivo = require('./lista.json'); //Desde que arquivo.js esteja na mesma pasta]
 
-var arquivo = require('./lista.json'); //Desde que arquivo.js esteja na mesma pasta
     server.get('/', function (req, res) {
-        
-    
         let concluidas = arquivo.filter((tarefa) => {
-            console.log(tarefa.id);           //aqui dentro tarefa existe
-            return tarefa.concluido === true;
+            return tarefa.id === 2;
          })
      
          console.log(concluidas);
          res.send(arquivo);
-
 })
-
-
-
-
 
 server.listen(3000);
